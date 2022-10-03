47fdd2192d3fa13910b662dde50f2c181acfffb4
Kempirbaev Kairat <kairat-kempirbaev@users.noreply.github.com>
2022-10-02 17:17:44 -0700

    Fix #2: Navigation menu updates active page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  navLinks = [
+    { index: '00', link: 'home' },
+    { index: '01', link: 'destination' },
+    { index: '02', link: 'crew' },
+    { index: '03', link: 'technology' },
+  ];
+  active = 0;
   title = 'vercel';
-  mobileMenu:boolean=true;
+  mobileMenu: boolean = true;
 }
